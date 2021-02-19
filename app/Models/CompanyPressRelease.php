bd78343b43844c1210816c72b85d71bbd0ebb17c
Eduard <eduardstancu@yahoo.com>
2021-02-19 14:26:19 +0200

    press release modification

--- app/Models/CompanyPressRelease.php
+++ app/Models/CompanyPressRelease.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyPressRelease extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    protected $fillable =['company_id', 'title', 'text', 'enabled'];
 }
